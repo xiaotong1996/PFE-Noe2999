04f097e7886c4ff45e8f2b2faa58986a4f11423a
Lyoko <xuruitonglea@hotmail.com>
2019-10-08 10:30:10 +0200

    update GameManager

---
+++ PFE Noe2999/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager instance;
+    public static GameManager Instance { get; private set; }
+    
+    private Vaisseau vaisseau;
+    private Carte carte;
+    private Destination joueurDestination;
+
+    [SerializeField]
+    private int seaLevel = 10;
+    [SerializeField]
+    private int date = 0;
+
+    public Vaisseau Vaisseau { get => vaisseau; set => vaisseau = value; }
+    public Carte Carte { get => carte; set => carte = value; }
+    public Destination JoueurDestination { get => joueurDestination; set => joueurDestination = value; }
+
+    public  int SeaLevel  { get => seaLevel; set => seaLevel = value; }
+    public int Date { get => date; set => date = value; }
+
+
+    private void Awake()
+    {
+        Debug.Assert(GameManager.Instance == null);
+        GameManager.Instance = null;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ PFE Noe2999/Assets/Scripts/Vaisseau.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vaisseau : MonoBehaviour
+{
+    private static Vaisseau instance;
+    public static Vaisseau Instance { get; private set; }
+
+
+    //private List<Salle> salles; 
+    // public List<Salle> Salles { get => salles; set => Salles = value; }
+
+    [SerializeField]
+    private int energiemax = 10;
+    public int Energiemax { get => energiemax; set => energiemax = value; }
+
+    [SerializeField]
+    private int energie = 0;
+    public int Energie { get => energie; set => energie = value; }
+
+
+    [SerializeField]
+    private int nourritureMax = 10;
+    public int NourritureMax { get => nourritureMax; set => nourritureMax = value; }
+
+
+    [SerializeField]
+    private int nourritureVeg = 0;
+    public int NourritureVeg { get => nourritureVeg; set => energie = value; }
+
+    [SerializeField]
+    private int nourritureViande = 0;
+    public int NourritureViande { get => nourritureViande; set => nourritureViande = value; }
+
+
+    [SerializeField]
+    private int materiauxMax = 10;
+    public int MateriauxMax { get => materiauxMax; set => materiauxMax = value; }
+
+    [SerializeField]
+    private int materiaux = 0;
+    public int Materiaux { get => materiaux; set => materiaux = value; }
+
+
+    private void Awake()
+    {
+        Debug.Assert(Vaisseau.Instance == null);
+        Vaisseau.Instance = null;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
