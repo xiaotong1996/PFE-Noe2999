d9d40b2318f18288d4e503cd4d1a3646b03d9010
xiaotong1996 <chen.xiaotong@hotmail.com>
2019-12-03 01:29:14 +0100

    add 3D map

---
+++ Assets/Scripts/Scripts_Xiaotong/Map3D/SpawIsland.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawIsland : MonoBehaviour
+{
+    [SerializeField]
+    private int ileNumber;
+
+    public GameObject Earth;
+    public GameObject Spawned;
+
+    public int IleNumber { get => ileNumber; set => ileNumber = value; }
+
+    void Start()
+    {
+
+        var r = Earth.GetComponent<SphereCollider>().radius;
+
+        for (int i = 0; i < ileNumber; i++)
+        {
+
+            var origin = Earth.gameObject.transform.position;
+            var onEarth = Random.onUnitSphere * r * 100;
+
+            var spawnedIle = Instantiate(Spawned, onEarth, Quaternion.identity, Earth.transform) as GameObject;
+
+            //var x = Random.Range(-1f, 1f);
+            //var y = Random.Range(-1f, 1f);
+            //var z = Random.Range(-1f, 1f);
+
+            //var vec = new Vector3(x, y, z).normalized * r*100;
+
+            spawnedIle.transform.LookAt(Earth.transform.position);
+            spawnedIle.transform.rotation = spawnedIle.transform.rotation * Quaternion.Euler(90, 0, 0);
+
+        }
+    }
+
+}
